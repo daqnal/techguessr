8c8035fd421056fe6b1c13d356e1bafcc9aa802f
Daniel Durling <dxgd@pm.me>
2026-08-20 03:13:42 -0400

    change env var import once again

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,10 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
-import {
-  PUBLIC_SUPABASE_URL,
-  PUBLIC_SUPABASE_ANON_KEY,
-} from "$env/static/public";
+import { env } from "$env/dynamic/public";
 
-const supabaseUrl = PUBLIC_SUPABASE_URL;
-const supabasePublishableKey = PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = env.PUBLIC_SUPABASE_URL;
+const supabasePublishableKey = env.PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabasePublishableKey);
