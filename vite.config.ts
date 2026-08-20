5534b9f115fb95a4cacb442f134945635ffcd94b
Daniel Durling <dxgd@pm.me>
2026-08-20 02:13:29 -0400

    remove wrangler.toml to use auto generated

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
+import adapter from "@sveltejs/adapter-cloudflare";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
@@ -11,6 +12,7 @@ export default defineConfig({
         runes: ({ filename }) =>
           filename.split(/[/\\]/).includes("node_modules") ? undefined : true,
       },
+      adapter: adapter(),
     }),
   ],
   optimizeDeps: {
