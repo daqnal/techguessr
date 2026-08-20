88f88e48099dc9cb04b27ecbb298ab90cd1b59d7
Daniel Durling <dxgd@pm.me>
2026-08-20 02:34:41 -0400

    remove unnecessary static development images

--- svelte-kit/cloudflare/_app/immutable/chunks/BM7MApe6.js
+++
@@ -1,1 +0,0 @@
-import"./BRhBxtGT.js";import{i as e,n as t}from"./3U47DCgq.js";var n={get data(){return e.data},get error(){return e.error},get form(){return e.form},get params(){return e.params},get route(){return e.route},get state(){return e.state},get status(){return e.status},get url(){return e.url}};t.updated.check;var r=n;export{r as t};
